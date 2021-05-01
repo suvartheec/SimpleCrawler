7c5faf188a6666c65dad238f0aa50bf51b40995a
Suvarthee Chandra <suvartheechandra@gmail.com>
2021-05-01 19:14:19 +0530

    optimizations

--- src/main/java/com/practice/simplecrawler/utils/SimpleCrawlerUtil.java
+++ src/main/java/com/practice/simplecrawler/utils/SimpleCrawlerUtil.java
@@ -4,18 +4,16 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
@@ -23,15 +21,13 @@ import org.springframework.util.Assert;
 public class SimpleCrawlerUtil {
 	private Logger logger = LogManager.getLogger();
 
-
-
 	/**
 	 * @param urlString to crawl
 	 * @return the result as Map of urls that are accessible from the provided input and the summary of each url
 	 * @throws IOException
 	 */
 	public Map<String, PageContents> crawl(String urlString) throws IOException {
-		return crawl(urlString, null, Collections.synchronizedMap(new HashMap<>()));
+		return crawl(urlString, null, new ConcurrentHashMap<>());
 	}
 
 	/**
@@ -41,7 +37,7 @@ public class SimpleCrawlerUtil {
 	 * @return the result of this page
 	 * @throws IOException
 	 */
-	public Map<String, PageContents> crawl(String urlString, String parent, Map<String, PageContents> pageContentsMap) throws IOException {
+	public Map<String, PageContents> crawl(String urlString, String parent, Map<String, PageContents> pageContentsMap) {
 		Assert.hasLength(urlString, "url cannot be blank");
 		logger.debug("processing:{}",urlString);
 		try {
@@ -51,13 +47,11 @@ public class SimpleCrawlerUtil {
 			pageContentsMap.put(getLinkWithoutParameters(urlString), details);
 
 			//process all its children..
-			for (String internalLink : details.getInternalLinks()) {
-				//...but only if not done already
-				if(!pageContentsMap.containsKey(getLinkWithoutParameters(internalLink))) {
-					crawl(internalLink, urlString, pageContentsMap);
-				}
-			}
-		} catch (HttpStatusException e) {
+			details.getInternalLinks().stream()
+				.filter(s-> !pageContentsMap.containsKey(getLinkWithoutParameters(s))) //only if not processed already
+//				.parallel() //TODO this exception while using parallel stream: javax.net.ssl.SSLException: No PSK available. Unable to resume.
+				.forEach(link->crawl(link, urlString,pageContentsMap));
+		} catch (IOException e) {
 			// in case JSoup fails to connect to any url, log and move on
 			logger.error(e);
 		}
@@ -65,13 +59,17 @@ public class SimpleCrawlerUtil {
 	}
 
 	/**
-	 * @param internalLink any given link
+	 * @param link any given link
 	 * @return the link without the extra hashbangs and querystrings to avoid repeatations  
-	 * @throws MalformedURLException
 	 */
-	public String getLinkWithoutParameters(String internalLink) throws MalformedURLException {
-		URL url = new URL(internalLink);
-		return url.getProtocol()+"://"+url.getHost()+url.getPath();
+	public String getLinkWithoutParameters(String link)  {
+		try {
+			URL url = new URL(link);
+			return url.getProtocol()+"://"+url.getHost()+url.getPath();
+		} catch (MalformedURLException e) {
+			logger.error("not a proper url:"+link);
+			return link;
+		}
 	}
 
 	/**
@@ -80,24 +78,18 @@ public class SimpleCrawlerUtil {
 	 * @return the summary as {@link PageContents}
 	 */
 	public PageContents getPageContents(Document doc, String parent) {
-		List<String> internalLinks = new ArrayList<>();
-		List<String> externalLinks = new ArrayList<>();
-		List<String> staticResources = new ArrayList<>();
+		Set<String> internalLinks = new ConcurrentSkipListSet<>();
+		Set<String> externalLinks = new ConcurrentSkipListSet<>();
+		Set<String> staticResources = new ConcurrentSkipListSet<>();
 		String location = null;
 		if(doc!=null) {
 			try {
 				location = doc.location();
 				String baseUriOfDoc = getBaseUriFromUri(doc.baseUri());
 
-				for (Element element : doc.select("a[href]")) {
-					if(element.absUrl("href").startsWith(baseUriOfDoc))
-						internalLinks.add(element.absUrl("href"));
-					else
-						externalLinks.add(element.absUrl("href"));
-				}
-				for(Element element : doc.select("img")) {
-					staticResources.add(element.absUrl("src"));
-				}
+				internalLinks = doc.select("a[href]").parallelStream().filter(element->element.absUrl("href").startsWith(baseUriOfDoc)).map(e->e.absUrl("href")).collect(Collectors.toSet());
+				externalLinks = doc.select("a[href]").parallelStream().filter(element->!element.absUrl("href").startsWith(baseUriOfDoc)).map(e->e.absUrl("href")).collect(Collectors.toSet());
+				staticResources = doc.select("img").parallelStream().filter(element->!element.absUrl("src").startsWith(baseUriOfDoc)).map(e->e.absUrl("src")).collect(Collectors.toSet());
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			}
